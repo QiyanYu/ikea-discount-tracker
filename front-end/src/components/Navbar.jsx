0bd31adaca990de54db8e3080cdeceae42ee2fe9
Qiyan(Ryan) Yu <ryanyu@duck.com>
2023-07-08 15:51:38 -0700

    navbar add blur

--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,3 +1,14 @@
-export default function Navbar() {
-  return <div>Navbar</div>;
+import { HiMenu } from "react-icons/hi";
+export default function Navbar({ toggleSideBar }) {
+  return (
+    <div className="sticky top-0 flex h-16 justify-between border-b bg-gray-100  bg-opacity-50 backdrop-blur-sm backdrop-filter ">
+      <button
+        onClick={toggleSideBar}
+        className=" my-4 flex h-8 w-20 items-center justify-center border-r text-xl sm:hidden"
+      >
+        <HiMenu />
+      </button>
+      <div className="flex-1">abc abc</div>
+    </div>
+  );
 }
